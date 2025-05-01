3faf8a848efc6936946b9d6783bb1feb2cdefe62
焦丹杨 <1154206436@qq.com>
2025-05-01 17:24:31 +0800

    支持\\n 匹配\n

--- src/main/java/com/jody/regex/ASTPattern.java
+++ src/main/java/com/jody/regex/ASTPattern.java
@@ -1,25 +1,29 @@
 package com.jody.regex;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ASTPattern {
     final RegexToASTree regexToASTree;
     final Ast ast;
     final int modifiers;
 
-    public ASTPattern(RegexToASTree regexToASTree,int modifiers) {
+    public ASTPattern(RegexToASTree regexToASTree, int modifiers) {
         this.regexToASTree = regexToASTree;
         this.ast = regexToASTree.astTree();
         this.modifiers = modifiers;
     }
 
-    public static ASTPattern compile(String regex){
+    public static ASTPattern compile(String regex) {
         return compile(regex, 0);
     }
-    public static ASTPattern compile(String regex,int modifiers) {
+
+    public static ASTPattern compile(String regex, int modifiers) {
         Util.checkEmpty(regex);
         String preprocessed = preprocess(regex);
-        RegexToASTree regexToASTree = new RegexToASTree(preprocessed,modifiers != 0);
-        return new ASTPattern(regexToASTree,modifiers);
+        RegexToASTree regexToASTree = new RegexToASTree(preprocessed, modifiers != 0);
+        return new ASTPattern(regexToASTree, modifiers);
     }
 
     /**
@@ -30,33 +34,51 @@ public class ASTPattern {
      * unicode 长度
      */
     private static final int UNI_CODE_LEN = 4;
+
     /**
-     *预处理
+     * \ 开头的特殊字符
+     */
+    private static Map<Character, Character> specialCharsMap = new HashMap<>();
+
+    static {
+        specialCharsMap.put('n', '\n');
+        specialCharsMap.put('r', '\r');
+        specialCharsMap.put('f', '\f');
+        specialCharsMap.put('t', '\t');
+    }
+
+    /**
+     * 预处理
      * 1. 预处理 \\xAA
      * 2.  \\u0000
+     * 3. \n \r \f
      */
     private static String preprocess(String regex) {
         StringBuilder sb = new StringBuilder();
         int i = 0;
         while (i < regex.length()) {
-            if (regex.charAt(i) == '\\' && i+1 <regex.length()) {
-                if(regex.charAt(i + 1) == 'x'){
+            if (regex.charAt(i) == '\\' && i + 1 < regex.length()) {
+                Character nextCh = regex.charAt(i + 1);
+                if (nextCh == 'x') {
                     //读取16进制数字
-                    if (i + 1+ HEX_LEN >= regex.length()) {
+                    if (i + 1 + HEX_LEN >= regex.length()) {
                         throw new RuntimeException("错误的16进制序列");
                     }
                     char ch = (char) Integer.valueOf(regex.substring(i + 2, i + 2 + HEX_LEN), 16).intValue();
                     sb.append(ch);
                     i += 2 + HEX_LEN;
-                } else if(regex.charAt(i+1)=='u'){
+                } else if (nextCh == 'u') {
                     //读取16进制数字
-                    if (i + 1+ UNI_CODE_LEN >= regex.length()) {
+                    if (i + 1 + UNI_CODE_LEN >= regex.length()) {
                         throw new RuntimeException("错误的Unicode序列");
                     }
                     char ch = (char) Integer.valueOf(regex.substring(i + 2, i + 2 + UNI_CODE_LEN), 16).intValue();
                     sb.append(ch);
                     i += 2 + UNI_CODE_LEN;
-                } else{
+                } else if (specialCharsMap.containsKey(nextCh) && i > 0 && regex.charAt(i - 1) != '\\') {
+                    sb.append(specialCharsMap.get(nextCh));
+                    i += 2;
+                } else {
                     sb.append(regex.charAt(i));
                     i++;
                 }
@@ -67,6 +89,7 @@ public class ASTPattern {
         }
         return sb.toString();
     }
+
     public ASTMatcher matcher(String str) {
         return new ASTMatcher(this, str);
     }
