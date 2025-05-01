1270ab7f09f031d40fb8bdfc6d8d0670192ce0a7
焦丹杨 <1154206436@qq.com>
2025-05-01 16:55:20 +0800

    支持unicode字符串

--- src/main/java/com/jody/regex/ASTPattern.java
+++ src/main/java/com/jody/regex/ASTPattern.java
@@ -27,21 +27,39 @@ public class ASTPattern {
      */
     private static final int HEX_LEN = 2;
     /**
+     * unicode 长度
+     */
+    private static final int UNI_CODE_LEN = 4;
+    /**
      *预处理
      * 1. 预处理 \\xAA
+     * 2.  \\u0000
      */
     private static String preprocess(String regex) {
         StringBuilder sb = new StringBuilder();
         int i = 0;
         while (i < regex.length()) {
-            if (regex.charAt(i) == '\\' && i + 1 < regex.length() && regex.charAt(i + 1) == 'x') {
-                //读取16进制数字
-                if (i + 1+ HEX_LEN >= regex.length()) {
-                    throw new RuntimeException("错误的16进制序列");
+            if (regex.charAt(i) == '\\' && i+1 <regex.length()) {
+                if(regex.charAt(i + 1) == 'x'){
+                    //读取16进制数字
+                    if (i + 1+ HEX_LEN >= regex.length()) {
+                        throw new RuntimeException("错误的16进制序列");
+                    }
+                    char ch = (char) Integer.valueOf(regex.substring(i + 2, i + 2 + HEX_LEN), 16).intValue();
+                    sb.append(ch);
+                    i += 2 + HEX_LEN;
+                } else if(regex.charAt(i+1)=='u'){
+                    //读取16进制数字
+                    if (i + 1+ UNI_CODE_LEN >= regex.length()) {
+                        throw new RuntimeException("错误的Unicode序列");
+                    }
+                    char ch = (char) Integer.valueOf(regex.substring(i + 2, i + 2 + UNI_CODE_LEN), 16).intValue();
+                    sb.append(ch);
+                    i += 2 + UNI_CODE_LEN;
+                } else{
+                    sb.append(regex.charAt(i));
+                    i++;
                 }
-                char ch = (char) Integer.valueOf(regex.substring(i + 2, i + 2 + HEX_LEN), 16).intValue();
-                sb.append(ch);
-                i += 2 + HEX_LEN;
             } else {
                 sb.append(regex.charAt(i));
                 i++;
